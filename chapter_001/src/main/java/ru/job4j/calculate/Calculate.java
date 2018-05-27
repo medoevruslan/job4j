a3dea21aef6def0a0501723b9916a245637bfdf0
RuslanMedoev <mr.r.m3@icloud.com>
2018-05-27 17:37:20 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,29 @@
 package ru.job4j.calculate;
 
-public class Calculate {
+/**
+* Package for calculate task.
+*
+* @author Medoev Ruslan (mr.r.m3@icloud.com)
+* @version $Id$
+* @since 0.1
+*/
 
+public class Calculate {
+/**
+* Main.
+* @param args - args.
+*/
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
-  
+
+/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+}
+
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Test.
+*
+* @author Medoev Ruslan (mr.r.m3@icloud.com)
+* @version $Id$
+* @since 0.1
+*/
+
+package ru.job4j.calculate;

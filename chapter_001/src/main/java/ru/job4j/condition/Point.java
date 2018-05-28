6d015e3809c2bb810a287b4379e2527a30f9b763
RuslanMedoev <mr.r.m3@icloud.com>
2018-05-28 13:48:31 +0300

    3.4. Расстояние между точками в системе координат [#188]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,30 @@
+package ru.job4j.condition;
+
+/**
+ * @author Medoev Ruslan (mr.r.m3@icloud.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(3, 7);
+        Point b = new Point(4, 9);
+
+        double result = a.distanceTo(b);
+
+        System.out.println("Расстояние между точками А и В : " + result);
+    }
+}

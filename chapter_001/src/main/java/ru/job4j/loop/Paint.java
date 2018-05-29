4b13fd2e7d51387885baaba0d08982d9f9b48104
RuslanMedoev <mr.r.m3@icloud.com>
2018-05-29 22:59:28 +0300

    5.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * @author Medoev Ruslan (mr.r.m3@icloud.com)
  * @version $Id$
@@ -9,17 +11,16 @@ package ru.job4j.loop;
 public class Paint {
 
     /**
-     * Метод посроения пирамиды в псевографике.
+     * Метод построения пирамиды в псевографике.
      * @param height Высота пирамиды.
      * @return Отображение пирамиды в псевлографике.
      */
 
-    public String pyramid(int height) {
+    private String loopBy(int height, int width, BiPredicate<Integer, Integer> biPredicate) {
         StringBuilder stringBuilder = new StringBuilder();
-        int width = height * 2 - 1;
         for (int row = 0; row < height; row++) {
             for (int column = 0; column < width; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (biPredicate.test(row, column)) {
                     stringBuilder.append("^");
                 } else {
                     stringBuilder.append(" ");
@@ -28,5 +29,13 @@ public class Paint {
             stringBuilder.append(System.lineSeparator());
         }
         return stringBuilder.toString();
+    }
+
+    public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                height * 2 - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
     }
 }

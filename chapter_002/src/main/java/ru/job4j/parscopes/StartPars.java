faeb9cb4c5e2d195d869d0898c70f36d7b60bab8
RuslanMedoev <mr.r.m3@icloud.com>
2018-06-20 01:56:54 +0300

    Распарсить скобки. [#34113]

--- chapter_002/src/main/java/ru/job4j/parscopes/StartPars.java
+++ chapter_002/src/main/java/ru/job4j/parscopes/StartPars.java
@@ -1,6 +1,6 @@
 package ru.job4j.parscopes;
 
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * @author Medoev Ruslan (mr.r.m3@icloud.com)
@@ -9,14 +9,21 @@ import java.util.LinkedList;
  */
 
 public class StartPars {
+   private ArrayList<Brackets> list = new ArrayList<>();
+   private Stacker stacker = new Stacker();
 
     public void init(String input) {
-        LinkedList<Brackets> list = new LinkedList<>();
-        Stacker stacker = new Stacker();
-        if (!stacker.checkAndParse(input, list )) {
+        if (!this.stacker.checkAndParse(input, this.list)) {
             System.out.println("Invalid brackets");
         } else {
-            for (Brackets br : list) {
+            System.out.println("String to parse - " + input +"\nresult : \n");
+            this.list.sort(new Comparator<Brackets>() {
+                @Override
+                public int compare(Brackets first, Brackets second) {
+                    return first.getKind().compareTo(second.getKind());
+                }
+            });
+            for (Brackets br : this.list) {
                 System.out.println(br.getKind() + " - open: " + br.start + " close: " + br.end);
             }
         }

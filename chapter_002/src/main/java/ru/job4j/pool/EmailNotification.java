59b0121c3e8956ec8db0fe9366268cbe38437108
RuslanMedoev <mr.r.m3@icloud.com>
2018-07-27 13:35:33 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ chapter_002/src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,64 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Medoev Ruslan (mr.r.m3@icloud.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class EmailNotification {
+    private String subject;
+    private String body;
+
+    public void emailTo(User user) {
+        this.subject  = "Notification for " + user.name + " to email " + user.email;
+        this.body = "New event for " + user.name;
+    }
+
+    public void send(String subject, String body, String email) { }
+
+    public void init(User user) {
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                emailTo(user);
+                this.notifyAll();
+            }
+        });
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                while (subject == null && body == null) {
+                    try {
+                        this.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                send(subject, body, user.email);
+            }
+        });
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class User {
+        private String name;
+        private String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+}

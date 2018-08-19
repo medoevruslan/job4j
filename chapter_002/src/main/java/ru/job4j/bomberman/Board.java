59d0a48e5c00b5ec4b3b2486f0107f7e4e020867
RuslanMedoev <mr.r.m3@icloud.com>
2018-08-19 17:03:54 +0300

    Игра бомбермен. [#1108]

--- chapter_002/src/main/java/ru/job4j/bomberman/Board.java
+++ chapter_002/src/main/java/ru/job4j/bomberman/Board.java
@@ -12,18 +12,35 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 
 public class Board {
-    private final ReentrantLock[][] board = new ReentrantLock[10][10];
+    private ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private int enemies = 1;
+    private int difficult = 10;
+    private final ReentrantLock[][] board = new ReentrantLock[difficult][difficult];
 
-    private Runnable bomber = new Bomberman();
-    private ExecutorService service = Executors.newSingleThreadExecutor();
+    public Board(int enemies, int difficult) {
+        this.enemies = enemies;
+        this.difficult = difficult;
+        }
+
+    public Board() { }
+
+    /**
+     * method initializing enemies
+     */
 
     public void init() {
-        service.submit(bomber);
+        for (int i = 0; i < enemies; i++) {
+            service.submit(new Enemie(Cell.values()[i]));
+        }
     }
 
-    private class Bomberman implements Runnable {
+    private class Enemie implements Runnable {
         private Cell position;
         private Cell destination;
+
+        public Enemie(Cell startCell) {
+            this.position = startCell;
+        }
 
         /**
          * Method search for possible direction of object movement.
@@ -38,9 +55,9 @@ public class Board {
             while (!pass) {
                 int x = (int) (Math.random() * 3);
                 int y = (int) (Math.random() * 3);
-                for (Cell cell : Cell.values()) {
-                    if (source.x + moves[x] == cell.x && source.y + moves[y] == cell.y) {
-                        result = cell;
+                for (int idx = 0; idx < Math.pow(difficult, 2); idx++) {
+                    if (source.x + moves[x] == Cell.values()[idx].x && source.y + moves[y] == Cell.values()[idx].y) {
+                        result = Cell.values()[idx];
                         pass = true;
                         break;
                     }
@@ -76,7 +93,6 @@ public class Board {
 
         @Override
         public void run() {
-            move(Cell.A1, destination);
             while (true) {
                 move(position, destination);
                 try {
@@ -85,6 +101,31 @@ public class Board {
                     ie.printStackTrace();
                 }
             }
+        }
+    }
+
+    private class Bomberman {
+
+        /**
+         * Player move
+         * @param source current source of object.
+         * @param dest destination to move object.
+         * @return boolean result of move
+         */
+
+        public boolean move(Cell source, Cell dest) {
+            boolean result = false;
+            ReentrantLock lock =  board[source.x][source.y];
+            lock.lock();
+            try {
+               if (board[dest.x][dest.y].tryLock(500, TimeUnit.MILLISECONDS)) {
+                   lock.unlock();
+                   result = true;
+               }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return result;
         }
     }
 }
